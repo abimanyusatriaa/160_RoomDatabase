8f5f94c40159d41a2baae90d86d0865ffcd10b52
abimanyusatriaa <adji3890@gmail.com>
2025-11-28 13:20:11 +0700

    membuat isi file container app

---
+++ app/src/main/java/com/example/room/repositori/ContainerApp.kt
@@ -0,0 +1,32 @@
+package com.example.room.repositori
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Database
+import com.example.room.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+class AplikasiSiswa : Application() {
+    /**
+     * Appcontainer instance digunakan oleh kelas-kelas lainnya untuk
+     * mendapatkan dependensi
+     */
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
