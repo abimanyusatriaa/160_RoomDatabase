dbb6057af728cf423a5112bef04e929db1a15f99
abimanyusatriaa <adji3890@gmail.com>
2025-11-28 13:20:32 +0700

    membuat file repositori siswa

---
+++ app/src/main/java/com/example/room/repositori/RepositoriSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.room.repositori
+
+import com.example.room.room.Siswa
+import com.example.room.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
