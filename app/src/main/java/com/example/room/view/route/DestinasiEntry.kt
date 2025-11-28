8ad16dd9b2d2f138d03335d5da129413f76a4f44
abimanyusatriaa <adji3890@gmail.com>
2025-11-28 13:22:55 +0700

    membuat isi file destinasi entry

---
+++ app/src/main/java/com/example/room/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.room.view.route
+
+import com.example.room.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
