3606e0680cd4766ea65c1327512f7b6960e65b79
abimanyusatriaa <adji3890@gmail.com>
2025-11-28 13:23:17 +0700

    membuat isi file destinasi home

---
+++ app/src/main/java/com/example/room/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.room.view.route
+
+import com.example.room.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
